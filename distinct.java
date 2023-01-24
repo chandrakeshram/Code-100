25731667b60df6cd8b2b35e1564b71d0a644c790
Chandrakesh Ram <79987328+chandrakeshram@users.noreply.github.com>
2023-01-24 09:41:30 +0530

    Day 1 Completed with Full Revision of Data Types and Their Basics

---
+++ distinct.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class distinct{
+    public static void main(String[] args) {
+        //Data types in Java
+        Scanner cs = new Scanner(System.in);
+        boolean isPrime=true;    //Size --> 1 Bit 
+        System.out.println(isPrime);
+        char ch = '\u0fff';           //Size --> 2 Bytes 
+        System.out.println(ch);
+        byte a = -128;           //Size --> 1 Byte
+        System.out.println(a);
+        short d = -32768;
+        int king =3 ;
+        System.out.println(king );
+        int maxLimit = 2147483647;
+        System.out.println(maxLimit);
+        long maximum = 2114783647;
+        int hex = 0x233;       //Integer Literal
+        int binary = 0b01011;  //Integer Literal 
+        System.out.println(0b10010111);
+        System.out.println(hex);
+        System.out.println(binary);
+        double doub = 322.e33;   //Double Literal 
+        System.out.println(doub);
+        char e = '\u0043';
+        System.out.println(e);
+        /* 1 Nibble = 4 Bits 
+         * 1 Byte = 8 Bits
+         * int :
+         * Size: 4 Bytes
+         * Range: -2^31 to 2^31 - 1
+         * 
+         * ========================
+         * char : 
+         * Size : 2 Bytes
+         * Range: -2^15 to 2^15 - 1
+         * 
+         * ========================
+         * boolean :
+         * Size : 1 Bit
+         * 
+         * ========================
+         * long :
+         * Size : 8 Bytes 
+         * Range: -2^63 to 2^63 - 1
+         * 
+         * Note:double
+         * i.e 
+         * for integer,
+         * Range: 0 to 2^32 - 1
+         *  
+         */
+        
+    }
+
+}
